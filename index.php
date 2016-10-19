7f88c50f6dcdadbfdead1eb43da1547f2a8fc36e
brianezequielfiszman <brianezequielfiszman@gmail.com>
2016-10-19 03:43:01 -0300

    Corrected const HOME

--- index.php
+++ index.php
@@ -5,17 +5,18 @@
     <title></title>
   </head>
   <body>
-    <?php
-    if (!isset($_GET['id']))
-      $_GET['id'] = HOME;
-    $config = include $_SERVER['DOCUMENT_ROOT'] . '/config/config.php';
-    
+    <?php 
     const HOME   = 'home';
     const FAQS   = 'faqs';
     const LOGIN  = 'login';
     const SIGNUP = 'signup';
 
+    if (!isset($_GET['id']))
+      $_GET['id'] = HOME;
+    $config = include $_SERVER['DOCUMENT_ROOT'] . '/config/config.php';
+    
     include $config['view']['URL']['head'];
+    
     switch ($_GET['id']):
       case HOME:
         include $config['view']['URL']['home'];
