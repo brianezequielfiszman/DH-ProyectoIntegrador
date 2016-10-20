ba834e402a66af4ce7e08f90ab24889d18a817f1
brianezequielfiszman <brianezequielfiszman@users.noreply.github.com>
2016-10-20 13:53:38 -0300

    Update submit_user.php

--- controller/submit_user.php
+++ controller/submit_user.php
@@ -4,9 +4,6 @@ $config = include $_SERVER['DOCUMENT_ROOT'] . '/config/config.php';
 include $config['model']['URL']['usuario'];
 include $config['controller']['URL']['repositorioJSON'];
 
-$jsonFile = $config['db']['json']['file_path'];
-$offset = $config['db']['json']['offset'];
-
 $jsonDB = new RepositorioJSON($config);
 
 $id = $jsonDB->repositorioUsuarios->getUsersCount() + 1;
