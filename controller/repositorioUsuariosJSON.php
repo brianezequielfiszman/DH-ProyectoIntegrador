12aebf9ec8c3b118dd7d4f0ce504f5a6387ec7aa
brianezequielfiszman <brianezequielfiszman@gmail.com>
2016-10-21 12:24:18 -0300

    Corregida el nombre de la interfaz implementada

--- controller/repositorioUsuariosJSON.php
+++ controller/repositorioUsuariosJSON.php
@@ -5,7 +5,7 @@ $config = include $_SERVER['DOCUMENT_ROOT'] . '/config/config.php';
 include $config['controller']['URL']['repositorioUsuarios'];
 include $config['controller']['URL']['repositorioGenericoJSON'];
 
-class RepositorioUsuariosJSON extends RepositorioGenericoJSON implements repositorioUsuarios
+class RepositorioUsuariosJSON extends RepositorioGenericoJSON implements RepositorioUsuarios
 {
     public function __construct($filePath, $offset)
     {
